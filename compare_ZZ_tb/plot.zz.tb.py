4e92a2ec625fb39d6ac5d2245530f63861e585ac
jihenghu <hjh18305@gmail.com>
2024-02-23 16:43:57 -0500

    Tb_deconvo_ZZ

---
+++ compare_ZZ_tb/plot.zz.tb.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+from scipy.special import legendre
+from scipy.optimize import curve_fit
+
+shape_func = pd.read_csv('./shape_fuc_ch6.csv')
+mu_=shape_func['mu'][:].values
+shape_=shape_func['shape'][:].values
+
+## fit the shape
+coeff0=[1.0]*2
+
+def Polynomials(x, a,b):
+    # result = np.zeros_like(x)
+    # for i, coeff in enumerate(coeffs):
+        # result += coeff * legendre(i)(x)
+    # return result
+    return 1/(1+np.exp(-1*a**2*(x+b)**2))
+
+# print(mu_)
+coeffs,pcor=curve_fit(Polynomials,mu_,shape_,p0=coeff0)
+
+
+## read in ZZ tb in channel 6
+tb_file=h5py.File(f"PJ51_Freq5.h5","r")
+c0_0=tb_file['ModelTypeupdate1_MultiPJ_Mode2/Iter2/c0'][:]
+c1=tb_file['ModelTypeupdate1_MultiPJ_Mode2/Iter2/c1'][:]
+c2=tb_file['ModelTypeupdate1_MultiPJ_Mode2/Iter2/c2'][:]
+
+## the north polar 
+c0=c0_0[-1]
+c1=c1[-1]
+c2=c2[-1]
+
+mu=np.linspace(1,0,100)
+base=c0-c1*(1-mu)/0.2+0.5*c2*(mu-0.2)*(1-mu)/0.04
+shapes=Polynomials(mu,*coeffs)
+tb=base*shapes
+
+fig, axes = plt.subplots(ncols=2,nrows=1,figsize=(20, 10))
+
+ax = axes[0]
+ax.set_xlim(1,0)
+# ax.scatter(mu_, shape_, c='blue' ,label="Tb _ch6")
+ax.plot(mu,base, c='blue' ,label="Tb _ch6 ")
+ax.grid()
+ax.legend()
+ax.set_title(f'a. Tb PJ51 CH6 north pole',fontsize=25,loc="left")
+
+ax = axes[1]
+lats=np.linspace(0,180,180)-89.5
+ax.plot(lats, c0_0, c='red' ,label="C0 _ch6")
+ax.grid()
+ax.legend()
+ax.set_title(f'b. C0 PJ51 CH6',fontsize=25,loc="left")
+
+
+
+fig.savefig(f"tb_zz_ch6_pj51.png",dpi=300)
+
