2a49c10de4388f3c691837c5d8b9ce1b56acfb5d
jihenghu <hjh18305@gmail.com>
2024-02-23 17:34:03 -0500

    regularly-save-progress

--- compare_ZZ_tb/plot.zz.tb.py
+++ compare_ZZ_tb/plot.zz.tb.py
@@ -15,15 +15,17 @@ shape_=shape_func['shape'][:].values
 ## fit the shape
 coeff0=[1.0]*2
 
-def Polynomials(x, a,b):
-    # result = np.zeros_like(x)
-    # for i, coeff in enumerate(coeffs):
-        # result += coeff * legendre(i)(x)
-    # return result
+def legendre_poly(x, a,b):
+    result = np.zeros_like(x)
+    for i, coeff in enumerate(coeffs):
+        result += coeff * legendre(i)(x)
+    return result
+
+def logistic(x, a,b):
     return 1/(1+np.exp(-1*a**2*(x+b)**2))
 
 # print(mu_)
-coeffs,pcor=curve_fit(Polynomials,mu_,shape_,p0=coeff0)
+coeffs,pcor=curve_fit(logistic,mu_,shape_,p0=coeff0)
 
 
 ## read in ZZ tb in channel 6
@@ -39,7 +41,7 @@ c2=c2[-1]
 
 mu=np.linspace(1,0,100)
 base=c0-c1*(1-mu)/0.2+0.5*c2*(mu-0.2)*(1-mu)/0.04
-shapes=Polynomials(mu,*coeffs)
+shapes=logistic(mu,*coeffs)
 tb=base*shapes
 
 fig, axes = plt.subplots(ncols=2,nrows=2,figsize=(20, 20))
@@ -52,14 +54,12 @@ ax.grid()
 ax.legend()
 ax.set_title(f'a. Shape function CH6',fontsize=25,loc="left")
 
-
 ax = axes[0,1]
 lats=np.linspace(0,180,180)-89.5
 ax.plot(lats, c0_0, c='red' ,label="C0 _ch6")
 ax.grid()
 ax.legend()
 ax.set_title(f'b. C0 PJ51 CH6',fontsize=25,loc="left")
-
 
 ax = axes[1,0]
 ax.set_xlim(1,0)
@@ -68,15 +68,12 @@ ax.grid()
 ax.legend()
 ax.set_title(f'c. Base function north pole',fontsize=25,loc="left")
 
-
 ax = axes[1,1]
 ax.set_xlim(1,0)
 ax.plot(mu,tb, c='blue' ,label="Tb _ch6 ")
 ax.grid()
 ax.legend()
 ax.set_title(f'd. Tb PJ51 CH6 north pole',fontsize=25,loc="left")
-
-
 
 fig.savefig(f"tb_zz_ch6_pj51.png",dpi=300)
 
